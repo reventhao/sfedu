81d51a92ba7ecc21e717fa7a8b47f65fddc3f69c
abc <ren895873@gmail.com>
2018-09-05 14:27:44 +0800

    11

--- src/main/java/com/sf/edu/SfeduApplication.java
+++ src/main/java/com/sf/edu/SfeduApplication.java
@@ -1,9 +1,11 @@
 package com.sf.edu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.sf.edu.mapper")
 public class SfeduApplication {
 
     public static void main(String[] args) {
