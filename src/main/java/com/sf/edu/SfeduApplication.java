1e45127f77b41f23e43a61efb6396a093262048e
abc <ren895873@gmail.com>
2018-09-05 12:50:44 +0800

    first commit

---
+++ src/main/java/com/sf/edu/SfeduApplication.java
@@ -0,0 +1,12 @@
+package com.sf.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfeduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfeduApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sf/edu/SfeduApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sf.edu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfeduApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
