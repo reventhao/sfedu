fac6667fdc19068064397e68b87ebf4529d84cf9
abc <ren895873@gmail.com>
2018-09-05 15:01:39 +0800

    班级控制器路径修改

--- src/main/java/com/sf/edu/controller/ClassController.java
+++ src/main/java/com/sf/edu/controller/ClassController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "class")
+@RequestMapping(value = "class/")
 public class ClassController {
 
     private ClassService classService;
